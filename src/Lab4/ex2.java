010be17128e0d4599d720659e398230e4c9f333b
Labcom4 <darkeiaw28@gmail.com>
2019-01-10 15:23:58 +0700

    Lab4-5

---
+++ src/Lab4/ex2.java
@@ -0,0 +1,25 @@
+package Lab4;
+
+public class ex2 {
+    public static void main (String[] args) {
+
+        int x,y;
+        x = 5;
+        y = 10;
+        double a,b;
+        a = 5.5;
+        b = 20.20;
+        summation(x,y);
+        //overloading methods
+        summation(a,b);
+    }//main
+
+    private static void summation (int x, int y) {
+        int sum =x+y;
+        System.out.println("Summation of x and y = "+sum);
+    }//summation
+    private static void summation (double x, double y) {
+        double sum = x + y;
+        System.out.println("Summation of x and y = " + sum);
+    }
+}//class
